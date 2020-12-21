035bcac215b65e64e6a93bfafb01fa99595b6af8
zhongll <1375125519@qq.com>
2020-12-21 23:42:23 +0800

    登录表单

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,15 +1,38 @@
 <template>
-    <div>
-      <el-row>aaaaa</el-row>
+    <div class="main">
+      <el-form :form="form" :rules="{}">
+        <el-input placeholder="请输入账号" v-model="form.account">
+          <template #suffix></template>
+        </el-input>
+        <el-input placeholder="请输入密码" v-model="form.password">
+          <template #suffix icon="el-icon-user"></template>
+        </el-input>
+      </el-form>
     </div>
 </template>
 
 <script>
 export default {
-  name: 'home'
+  name: 'home',
+  data() {
+    return {
+      form:{
+        account:'', //账号
+        password:'', //密码
+      }
+
+    }
+  }
 }
 </script>
-
-<style scoped>
-
+<style lang="scss">
+  .main {
+    /*background-image: url("../assets/img/login-bg.jpg");*/
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    form {
+      padding:30px;
+    }
+  }
 </style>
